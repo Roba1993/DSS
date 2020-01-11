3761bc871d2954eaf9efafb4d6c4312bd8508d9b
Robert Schütte <schuetterobert@gmail.com>
2020-01-11 14:26:22 +0100

    Example bug fiy and cargo toml update

--- examples/light.rs
+++ examples/light.rs
@@ -3,5 +3,5 @@ fn main() {
     let  appt = dss::Appartement::connect("dss", "dssadmin", "dssadmin").unwrap();
 
     // turn the light in the zone 2 and group 0 on
-    appt.set_value(2, 0, dss::Value::Light(1))?;
+    appt.set_value(2, Some(0), dss::Value::Light(1.0)).unwrap();
 }
